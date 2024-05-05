2374a431d5a38fe70ecb0a07726a17a12dbf1f13
lpdmy <dieumy430@gmail.com>
2024-05-05 19:21:07 +0700

    First commit

---
+++ Services/Cabinet.cs
@@ -0,0 +1,17 @@
+﻿namespace Services
+{
+    public class Cabinet<T>// COI DATATYPE LÀ THAM SỐ TRUYỀN VÀO
+    {
+        private T[] _list1 = new T[300];
+        private int _count1 = 0;
+
+        public void AddObject()
+        {
+
+        }
+        public void PrintObject()
+        {
+
+        }
+    }
+}
